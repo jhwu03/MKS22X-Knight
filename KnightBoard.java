333bca39ecc71befa87015a9e0516c0f5e19f74f
Jionghao <jwu03@stuy.edu>
2019-02-13 11:50:42 -0500

    start

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,5 +1,7 @@
 public class KnightBoard{
   int[][] board;
+  int[8] xMove = {1,2,2,1,-1,-2,-2,-1}
+  int[8] yMove = {2,1,-1,-2,-2,-1,1,2}
   public KnightBoard(int startingRows,int startingCols){
     board = new int[startingRows][startingCols];
     for(int i =0; i < board.length; i++){
@@ -7,6 +9,20 @@ public class KnightBoard{
         board[i][r] = 0;
       }
     }
+  }
+
+  public boolean solve(int startingRows, int startingCols){
+    if(startingRows > board.length || startingCols > board[0].length
+       || startingRows < 0 || startingCols < 0) throw new IllegalStateException();
+
+    for(int i =0; i < board.length; i++){
+      for(int r = 0; r < board[0].length; r++){
+        if(board[i][r] != 0){
+          throw new IllegalStateException();
+        }
+      }
+    }
+    return solveHelper()
   }
 
 }
