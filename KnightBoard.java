43aa26427fd0939395d1f6eb32f6cc0bd965df4c
Jionghao <jwu03@stuy.edu>
2019-02-14 16:29:42 -0500

    added tostring and finished solve need to test

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,11 +1,13 @@
 public class KnightBoard{
   int[][] board;
-  int size = board.length;
-  int size2 = board[0].length;
+  int size;
+  int size2;
   int[] xMove = {1,2,2,1,-1,-2,-2,-1};
   int[] yMove = {2,1,-1,-2,-2,-1,1,2};
-  public KnightBoard(int startingRows,int startingCols){
-    board = new int[startingRows][startingCols];
+  public KnightBoard(int rows,int cols){
+    board = new int[rows][cols];
+    size = board.length;
+    size2 = board[0].length;
     for(int i =0; i < board.length; i++){
       for(int r = 0; r < board[0].length; r++){
         board[i][r] = 0;
@@ -14,7 +16,11 @@ public class KnightBoard{
   }
 
   public boolean addKnight(int x, int y,int moveNumber){
-    board[x][y] = moveNumber;
+    if(x >= 0 && x < size && y >= 0 && y < size2 && board[x][y] == 0){
+         return true;
+       }
+    board[x][y] += moveNumber;
+    return false;
   }
 
   public boolean solve(int startingRows, int startingCols){
@@ -39,14 +45,36 @@ public class KnightBoard{
     for(int i = 0; i < 8 ; i++){
       xcor = startingRows + xMove[i];
       ycor = startingCols + yMove[i];
-      if(xcor >= 0 && xcor < size && ycor >= 0 && ycor < size2 && board[xcor][ycor] == 0){
-        addKnight(xcor,ycor,moveNumber);
+      if(addKnight(xcor,ycor,moveNumber)){
         if(solveHelper(startingRows,startingCols,xMove,yMove,moveNumber + 1)){
           return true;
         }
         board[xcor][ycor] = 0;
       }
     }
+    return false;
+  }
+
+  public String toString(){
+    String ans = "";
+    for(int i =0; i < board.length; i++){
+      for(int r = 0; r < board[0].length; r++){
+        if(board[i][r] == 0){
+          ans += " _";
+        }else{
+          ans += "" + board[i][r];
+        }
+      }
+      ans += "\n";
+    }
+    return ans;
+
+  }
+
+  public static void main(String[] args) {
+    KnightBoard board = new KnightBoard(8,8);
+    System.out.println(board.solve(0,0));
+    System.out.println(board);
 
   }
 
