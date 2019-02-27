3998723ad36153bcee8dda3511f88d8e7d8d002a
jwu03 <jwu03@stuy.edu>
2019-02-26 21:38:35 -0500

    fixed solve and finished count

--- KnightBoard.java
+++ KnightBoard.java
@@ -27,6 +27,17 @@ public class KnightBoard{
     return false;
   }
 
+  private boolean removeKnight(int r, int c){
+  if (r < 0 || r >= board.length || c < 0 || c >= board.length){
+    return false;
+  }
+  if (board[r][c] == 0) {
+    return false;
+  }
+  board[r][c] = 0;
+  return true;
+}
+
   public boolean solve(int startingRows, int startingCols){
     if(startingRows > board.length || startingCols > board[0].length
        || startingRows < 0 || startingCols < 0) throw new IllegalStateException();
@@ -38,30 +49,34 @@ public class KnightBoard{
         }
       }
     }
-    addKnight(startingRows,startingCols,1);
-    return solveHelper(startingRows,startingCols,xMove, yMove,2);
+    //addKnight(startingRows,startingCols,1);
+    return solveHelper(startingRows,startingCols,xMove,yMove,1);
   }
 
   private boolean solveHelper(int startingRows, int startingCols, int[] xMove, int[] yMove, int moveNumber){
-    int xcor,ycor;
-    if(moveNumber > size * size){
+    if(moveNumber > size * size2){
       //System.out.println("" + size+ "," + moveNumber);
       return true;
-
+    }
+    if (startingRows < 0 || startingCols < 0 || startingRows >= size || startingCols >= size2) {
+      return false; // if out of bounds go back
+    }
+    if (board[startingRows][startingCols] != 0) {
+      return false;
+      // if this is a knight go back
     }
     for(int i = 0; i < 8 ; i++){
-      xcor = startingRows + xMove[i];
-      ycor = startingCols + yMove[i];
-      if(isSafe(xcor,ycor,board)){
-        board[xcor][ycor] = moveNumber;
-        if(solveHelper(xcor,ycor,xMove,yMove,moveNumber + 1)){
+        board[startingRows][startingCols] = moveNumber;
+        if(solveHelper(startingRows + xMove[i],startingCols + yMove[i],xMove,yMove,moveNumber + 1)){
           return true;
         }
-        board[xcor][ycor] = 0;
-      }
+        board[startingRows][startingCols] = 0;
     }
     return false;
   }
+
+
+
 
   public int countSolutions(int startingRow, int startingCol){
     if(startingRow > board.length || startingCol > board[0].length
@@ -74,28 +89,34 @@ public class KnightBoard{
          }
        }
      }
-     addKnight(startingRow,startingCol,1);
-     return countHelper(startingRow,startingCol, xMove, yMove,2);
+     //addKnight(startingRow,startingCol,1);
+     return countHelper(startingRow,startingCol, xMove, yMove,1);
   }
 
   private int countHelper(int startingRow, int startingCol, int[] xMove, int[] yMove, int moveNumber){
-    int xcor,ycor,ans;
-    if(moveNumber > size * size){
+
+    if (startingRow < 0 || startingCol < 0 || startingRow >= size || startingCol >= size2) {
+      return 0; // if out of bounds not a solution
+    }
+    if (board[startingRow][startingCol] != 0) {
+      return 0;
+      // if this is a knight not a solution
+    }
+    if(moveNumber == size * size2){
       //System.out.println("" + size+ "," + moveNumber);
       return 1;
-
     }
+    int ans = 0;
     for(int i = 0; i < 8 ; i++){
-      xcor = startingRows + xMove[i];
-      ycor = startingCols + yMove[i];
-      if(isSafe(xcor,ycor,board)){
-        board[xcor][ycor] = moveNumber;
-          return ans += countHelper(startingRow,startingCol, xMove, yMove,moveNumber + 1);
-        }
-        board[xcor][ycor] = 0;
-      }
-      return ans;
+        board[startingRow][startingCol] = moveNumber;
+        ans += countHelper(startingRow + xMove[i],startingCol + yMove[i],xMove,yMove,moveNumber + 1);
+        board[startingRow][startingCol] = 0;
     }
+    return ans;
+    }
+
+
+
 
   public String toString(){
     String ans = "";
@@ -114,8 +135,8 @@ public class KnightBoard{
   }
 
   public static void main(String[] args) {
-    KnightBoard board = new KnightBoard(8,8);
-    System.out.println(board.solve(0,0));
+    KnightBoard board = new KnightBoard(5,6);
+    System.out.println(board.countSolutions(0,0));
     System.out.println(board);
 
   }
